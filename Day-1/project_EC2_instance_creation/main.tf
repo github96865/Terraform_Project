caca025d188f6409939e8bbf74c3b22dc14fc0d3
gopal263 <venu.gopal@alethea.in>
2024-12-27 13:30:25 +0530

    removed .terraform folder to avoid the size issues.

---
+++ Day-1/project_EC2_instance_creation/main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+resource "aws_instance" "example" {
+    ami           = "ami-0e2c8caa4b6378d8c"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
